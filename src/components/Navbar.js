5eb9c5dcf4138f209cda16e67a491f22b0eb2882
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2023-10-30 10:34:32 -0400

    Created navbar with colour pallete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,30 @@
-import React from "react";
+import './Navbar.css';
 
-export default function Navbar(){
+ 
+
+function Navbar(){
     return(
+
+        <>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"/>
         <section className="navbar-section">
             <div className="home">
-                <h1>Tyler Philip</h1>
+                
+                <a className = "home-link" href="/home"><h1>Home Button</h1></a>
+            </div>
+            <div className="pages">
+                
+                <ul className="menu">
+                    <li className="page-link" ><a className="page-text" href="/contact">Contact</a></li>
+                    <li className="page-link" ><a className="page-text" href="/projects">Projects</a></li>
+                    <li className="page-link" ><a className="page-text" href="/about">About</a></li>
+                </ul>
             </div>
         </section>
+        </>
     )
 }
+
+export default Navbar;
